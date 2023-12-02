9a1e6ffc0a1372d39621ec451dc4ded5e7221397
Marina <iriciucmarina@yahoo.com>
2023-12-02 13:59:20 +0200

    change prisma file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,16 +2,16 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
 } else {
-  // @ts-ignore
-    if (!global.prisma) {
-        // @ts-ignore
-      global.prisma = new PrismaClient();
-    }
-  // @ts-ignore
-    prisma = global.prisma;
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
