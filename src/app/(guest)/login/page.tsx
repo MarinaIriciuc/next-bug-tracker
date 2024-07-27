29ebae5e7f6becc7f6a2e1eef88febeaf66a19c8
Marina <iriciucmarina@yahoo.com>
2024-07-27 22:45:41 +0300

    I hid the register button

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -21,13 +21,13 @@ export default async function Login() {
                         <p className="focus:outline-none text-2xl font-extrabold leading-6 text-gray-800">
                             Login to your account
                         </p>
-                        <p className="focus:outline-none text-sm mt-4 font-medium leading-none text-gray-500">
-                            Dont have account?{" "}
-                            <Link href="/register"
-                                className="hover:text-gray-500 focus:text-gray-500 focus:outline-none focus:underline hover:underline text-sm font-medium leading-none text-gray-800 cursor-pointer">
-                                Sign up here
-                            </Link>
-                        </p>
+                        {/*<p className="focus:outline-none text-sm mt-4 font-medium leading-none text-gray-500">*/}
+                        {/*    Dont have account?{" "}*/}
+                        {/*    <Link href="/register"*/}
+                        {/*        className="hover:text-gray-500 focus:text-gray-500 focus:outline-none focus:underline hover:underline text-sm font-medium leading-none text-gray-800 cursor-pointer">*/}
+                        {/*        Sign up here*/}
+                        {/*    </Link>*/}
+                        {/*</p>*/}
                         <LoginButton/>
                     </div>
                 </div>
